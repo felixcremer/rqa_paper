6fb40a8fa783167976f8d98d7a151dce313a43e5
Felix Cremer <felix.cremer@uni-jena.de>
2019-04-04 15:38:47 +0200

    Change size of rpplots

--- rqa_paper_julia/plot_utils.jl
+++ rqa_paper_julia/plot_utils.jl
@@ -3,7 +3,7 @@ using RecurrenceAnalysis
 
 dB(x) = 10 * log10(x)
 
-function plot_ts_rp(dates, ts1, ts2)
+function plot_ts_rp(dates, ts1, ts2, outpath)
     rp1 = RecurrenceMatrix(dB.(ts1), 1)
     rp2 = RecurrenceMatrix(dB.(ts2), 1)
     figure(figsize=(12,8))
@@ -12,7 +12,8 @@ function plot_ts_rp(dates, ts1, ts2)
     subplot2grid((3,2), (0,1))
     plot(dates, dB.(ts2))
     subplot2grid((3,2), (1,0), rowspan=2)
-    imshow(grayscale(rp1))
+    imshow(grayscale(rp1), extent=(0,length(ts1), 0, length(ts1)), cmap="binary_r")
     subplot2grid((3,2), (1,1), rowspan=2)
-    imshow(grayscale(rp2))
+    imshow(grayscale(rp2), extent=(0,length(ts2), 0,length(ts2)), cmap="binary_r")
+    savefig(outpath)
 end
